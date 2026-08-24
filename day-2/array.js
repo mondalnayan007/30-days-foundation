cd50753fa8f85e3ed8ae5b8debbdff16ba5c0ca3
Nayan Mondal <n007kingkong@gmail.com>
2026-08-24 21:48:02 +0600

    practice array and its methods

---
+++ day-2/array.js
@@ -0,0 +1,12 @@
+
+
+
+const food = ['kacchi', 'chicken fry','burger', 'Pizza'];
+console.log(food);
+
+food.push('speed');
+food.pop();
+food.unshift('morog polao');
+food.splice(2,0,'tanduri');
+let newFood = food.slice(0,1)
+console.log(newFood);
