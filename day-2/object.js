c07ae78f7b23107f0c0837e541df9ee9c6305db4
Nayan Mondal <n007kingkong@gmail.com>
2026-08-24 22:01:05 +0600

    practice object and its methods

---
+++ day-2/object.js
@@ -0,0 +1,13 @@
+
+
+let user = {
+   name:'nayan',
+   age:20,
+   address:'Dhaka'
+}
+user.profession = "Student";
+
+console.log(user);
+console.log(Object.entries(user));
+console.log(Object.keys(user));
+console.log(Object.values(user));
